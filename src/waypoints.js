2a7f8254cffd2307287745f6a8f74aa2810b8650
smithdamen <smith.damen@gmail.com>
2015-08-09 21:18:17 +0000

    modified waypoints

--- src/waypoints.js
+++ src/waypoints.js
@@ -1,4 +1,6 @@
 //This script is for custom animations on title sections
+
+/*
 $(document).ready(function(){
     $(".title-section").waypoint(function(){
         $(".title-section").addClass("animated fadeInUp");
@@ -6,3 +8,30 @@ $(document).ready(function(){
         offset: '300'
     });
 });
+*/
+
+//This script is from http://jsfiddle.net/harconst/4FNFW/
+function onScrollInit( items, trigger ) {
+  items.each( function() {
+    var osElement = $(this),
+        osAnimationClass = osElement.attr('data-animation'),
+        osAnimationDelay = osElement.attr('data-animation-delay');
+      
+        osElement.css({
+          '-webkit-animation-delay':  osAnimationDelay,
+          '-moz-animation-delay':     osAnimationDelay,
+          'animation-delay':          osAnimationDelay
+        });
+
+        var osTrigger = ( trigger ) ? trigger : osElement;
+        
+        osTrigger.waypoint(function() {
+          osElement.addClass('animated').addClass(osAnimationClass);
+          },{
+              triggerOnce: true,
+              offset: '70%'
+        });
+  });
+}
+
+ onScrollInit( $('.title-section') );
