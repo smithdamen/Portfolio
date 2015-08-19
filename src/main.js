5128e0c4477f6d0abe405ab483468935069dd797
smithdamen <smith.damen@gmail.com>
2015-08-19 15:25:03 +0000

    modified scripts

---
+++ src/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+//Scrollto    
+    $('a[href^="#"]').click(function(e){
+        e.preventDefault();
+        // console.log(this.hash);
+        $(window).stop(true).scrollTo(this.hash, {duration:1000, interrupt:true, easing:'easeInBack'});
+    });
+//Stellar
+    $('.full-height').stellar();
+//MixItUp
+    $(function(){
+        $('#mix-container').mixItUp();
+    });
+//Lettering
+    $(".fancy_title").lettering();
+//FitText
+    $("#responsive_headline").fitText();
+//Masonry
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        percentPosition: true,
+        columnWidth: '.column-sizer'
+    });
+//ScrollSpy
+    $('body').scrollspy({ target: '#navbar', offset: -75 });
+});
