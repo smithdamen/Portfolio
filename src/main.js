a000998db387d51f21d05955992bd8d5bcca8e89
smithdamen <smith.damen@gmail.com>
2015-08-20 02:32:55 +0000

    modified scripts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+//Waypoints
+    var animationName="animated fadeInUp";
+    
+    $('#main-title').waypoint(function() {
+        $('#main-title').addClass(animationName);
+    });
+    $('#about-title').waypoint(function() {
+        $('#about-title').addClass(animationName);
+    } , { offset : '70%' });
+    $('#portfolio-title').waypoint(function() {
+        $('#portfolio-title').addClass(animationName);
+    } , { offset : '70%' });
+    $('#blog-title').waypoint(function() {
+        $('#blog-title').addClass(animationName);
+    } , { offset : '70%' });
+    $('#contact-title').waypoint(function() {
+        $('#contact-title').addClass(animationName);
+    } , { offset : '70%' });
 //Scrollto    
     $('a[href^="#"]').click(function(e){
         e.preventDefault();
